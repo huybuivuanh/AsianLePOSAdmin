7db8b3471daa4215644f8f3cd1b37885e81452a4
huybuivuanh <huy_buivu12345@yahoo.ca>
2025-10-01 12:12:17 -0600

    start user snapshot listener in root layout.tsx

--- src/app/providers/StoreProvider.tsx
+++ src/app/providers/StoreProvider.tsx
@@ -6,30 +6,35 @@ import { useCategoriesStore } from "@/app/store/useCategoriesStore";
 import { useOptionStore } from "@/app/store/useOptionStore";
 import { useOptionGroupStore } from "@/app/store/useOptionGroupStore";
 import { useItemStore } from "@/app/store/useItemStore";
+import { useUserStore } from "../store/useUserStore";
 
 export function StoreProvider({ children }: { children: React.ReactNode }) {
   const { subscribe: subscribeCategories } = useCategoriesStore();
   const { subscribe: subscribeItems } = useItemStore();
   const { subscribe: subscribeOptionGroups } = useOptionGroupStore();
   const { subscribe: subscribeOptions } = useOptionStore();
+  const { subscribe: subscribeUsers } = useUserStore();
 
   useEffect(() => {
     const unsubCategories = subscribeCategories();
     const unsubItems = subscribeItems();
     const unsubGroups = subscribeOptionGroups();
     const unsubOptions = subscribeOptions();
+    const unsubUsers = subscribeUsers();
 
     return () => {
       unsubCategories();
       unsubItems();
       unsubGroups();
       unsubOptions();
+      unsubUsers();
     };
   }, [
     subscribeCategories,
     subscribeItems,
     subscribeOptionGroups,
     subscribeOptions,
+    subscribeUsers,
   ]);
 
   return <>{children}</>;
