ae8d3fd4e7b389a1fed49cbb4ff96d4bc8728bde
huybuivuanh <huy_buivu12345@yahoo.ca>
2026-07-23 10:00:40 -0600

    update custom claim when updating users

--- src/app/api/users/updateUser/route.ts
+++ src/app/api/users/updateUser/route.ts
@@ -14,6 +14,8 @@ export async function POST(req: Request) {
       displayName: name,
     });
 
+    await adminAuth.setCustomUserClaims(uid, { role });
+
     // 2️⃣ Update Firestore
     await adminDb.collection("users").doc(uid).update({
       name,
